bc37e119edc1f5b75e2ddc4a6505596982407b67
j-himanshu <hs.9731391859@outlook.com>
2017-09-14 20:12:07 +0530

    fixed CSS

--- src/App.js
+++ src/App.js
@@ -4,25 +4,45 @@ import logo from './logo.png';
 
 const styles = {
     input : {
-        "width" : "400px",
+        "width" : "80%",
         "height": "30px",
         "marginTop" : "15px",
-        "fontSize" : "30px"
+        "fontSize" : "20px",
+        "color" : "red"
     },
 
     message : {
-        "width" : "400px",
+        "width" : "80%",
         "height": "100px",
         "marginTop" : "15px",
-        "fontSize" : "20px"
+        "fontSize" : "20px",
+        "color" : "blue"
     },
 
-    button : {
-        "width" : "400px",
+    button1 : {
+        "width" : "80%",
         "height": "40px",
         "fontSize" : "30px",
         "color" : "white",
         "background" : "blue",
+        "marginBottom" : "20px"
+    },
+
+    button2 : {
+        "width" : "80%",
+        "height": "40px",
+        "fontSize" : "30px",
+        "color" : "white",
+        "background" : "red",
+        "marginBottom" : "20px"
+    },
+
+    button3 : {
+        "width" : "80%",
+        "height": "40px",
+        "fontSize" : "30px",
+        "color" : "white",
+        "background" : "green",
         "marginBottom" : "20px"
     }
 };
@@ -33,13 +53,15 @@ class App extends Component {
         super(props);
         this.state = {
             senderEmail : null,
-            senderPassword : null,
+            passkey : null,
             receiverEmail : null,
             plainMessage : null,
-            secretMessage : null
+            secretMessage : null,
+            key : null
         };
 
         this.handleChange = this.handleChange.bind(this);
+        this.generateKey = this.generateKey.bind(this);
     };
 
     handleChange(event) {
@@ -48,9 +70,20 @@ class App extends Component {
         this.setState(change);
     }
 
+    generateKey() {
+        if(this.state.senderEmail === null || this.state.senderEmail.length < 6)
+            alert("Enter Your Email");
+        else if (this.state.passkey === null || this.state.passkey.length !== 8)
+            alert("Enter Password");
+        else if (this.state.receiverEmail == null || this.state.receiverEmail.length < 6)
+            alert("Enter Recipient Email");
+        else
+            alert("Please note down following private key: " + 12345678)
+    }
+
     render() {
         return (
-            <div>
+            <div className="Body">
                 <div className="App">
                     <div className="App-header">
                         <img src = {logo} alt = "keryx" width = "80px" height = "80px"/>
@@ -59,9 +92,9 @@ class App extends Component {
                 </div>
                 <div className="App">
                     <div className="panel">
-                        <input placeholder="Your eMail" type = "email" name = "senderEmail" value = {this.state.senderEmail} onChange={this.handleChange.bind(this)} style = {styles.input}/>
+                        <input placeholder="Your Gmail ID" type = "email" name = "senderEmail" value = {this.state.senderEmail} onChange={this.handleChange.bind(this)} style = {styles.input}/>
                         <br/>
-                        <input placeholder="Your Password" type = "password" name = "senderPassword" value = {this.state.senderPassword} onChange={this.handleChange.bind(this)} style = {styles.input}/>
+                        <input placeholder="Gmail Password / Private Key" type = "password" name = "passkey" value = {this.state.passkey} onChange={this.handleChange.bind(this)} style = {styles.input}/>
                         <br/>
                         <input placeholder="Receiver's eMail" type = "email" name = "receiverEmail" value = {this.state.receiverEmail} onChange={this.handleChange.bind(this)} style = {styles.input}/>
                         <hr/>
@@ -71,16 +104,16 @@ class App extends Component {
                         <textarea placeholder="Enter Secret Message" type = "textbox" name = "secretMessage" value = {this.state.secretMessage} onChange={this.handleChange.bind(this)} style = {styles.message}/>
                         <hr/>
 
-                        KEY / AudioFile
+                        Key Image / Audio File
                         <br/>
                         <input type = "file" name = "upload" style = {styles.input}/>
                         <hr/>
 
-                        <input type = "button" style = {styles.button} value = "Send Key"/>
+                        <input type = "button" style = {styles.button1} value = "🔑 Generate Key 🔒" onClick={()=>this.generateKey()}/>
                         <br/>
-                        <input type = "button" style = {styles.button} value = "Send Message"/>
+                        <input type = "button" style = {styles.button2} value = "📧 Send Message "/>
                         <br/>
-                        <input type = "button" style = {styles.button} value = "Decrypt"/>
+                        <input type = "button" style = {styles.button3} value = "🔓 Decrypt"/>
                         <br/>
                     </div>
                 </div>
@@ -88,7 +121,7 @@ class App extends Component {
                     Website developed by HMS Pvt. Ltd. (CSE - 18)
                     < br />
                     < a href="mailto:keryx.messenger@gmail.com?Subject=Hey%20there!%20Nice%20Application!">
-                        <font color="FF0000"> Want to reach out to us ? keryx.messenger@gmail.com </font>
+                        <font color="000090"> Want to reach out to us ? keryx.messenger@gmail.com </font>
                     </a>
                     <br/>
                     <br/>
